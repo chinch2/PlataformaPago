2ea55b40d4707dccde82437058dfe669f9861246
Jose Arguello <Johao5991@gmail.com>
2019-07-19 16:56:56 -0400

    VentaMIFARE

--- PuntoDeVenta/src/main.cpp
+++ PuntoDeVenta/src/main.cpp
@@ -158,7 +158,7 @@ void loop()
 		// Enviamos serialemente su UID Leer tarjeta
 		USE_SERIAL.print("Card UID:");
 		for (byte i = 0; i < mfrc522.uid.size; i++)
-		{
+		{	
 			USE_SERIAL.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
 			USE_SERIAL.print(mfrc522.uid.uidByte[i], HEX);
 		}
